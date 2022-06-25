962725315f327be8d5ba406ab7ffe075ae3e3805
bmgcosta <bmgcosta@outlook.com>
2022-06-25 16:36:04 +0100

    Product Service:
    -> method get():
    	- Try get data of database, if doesn't get data will go try get data the API Products, if doesn't get gives a error
    
    -> method save():
    	- Save data in Database or gives a error
    
    -> method update():
    	- Update data in Database or gives a error
    
    -> method delete():
    	- Delete data in Database or gives a error

---
+++ src/service/ProductService.js
@@ -0,0 +1,50 @@
+import ProductsAPI from "./api/ProductsAPI";
+import Database from "./database/Database";
+
+class ProductService {
+
+    static #bd = Database.init();
+
+    static async get(){
+        try {
+            return this.#bd.get('products');
+        } catch (e) {
+            try {
+                const data = await ProductsAPI.get();
+                data.forEach(function(product){
+                    ProductService.#bd.add('products', ProductsAPI.obj(product), true);
+                });
+                return this.#bd.get('products');
+            } catch (err) {
+                throw err;
+            }
+        }
+    }
+
+    static save(productObj){
+        try {
+            this.#bd.add('products', productObj, true);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static update(productObj){
+        try {
+            this.#bd.upd('products', productObj, true);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static delete(productObj){
+        try {
+            this.#bd.del('products', productObj);
+        } catch (e) {
+            throw e;
+        }
+    }
+
+}
+
+export default ProductService;

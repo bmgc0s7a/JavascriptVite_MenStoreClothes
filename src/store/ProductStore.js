a2cbf6ac3baa7b91f552940846ccc06d0a385a04
bmgcosta <bmgcosta@outlook.com>
2022-06-26 16:01:37 +0100

    Product Store
    -> Array instances Product Model
    -> Method getAll():
    	- Return all content of array store if doen't has any value, try get data of Database
    -> Method get():
    	- Return a object Product of array products, if doen's has any value will go return exception
    -> Method add():
    	- This is internal method and create new instance Product Model with object recive, push for array and return that instance Product
    -> Method save():
    	- Call method add() and redirect this data for ProductBD.save() to insert in database
    -> Method update():
    	- Change value property and redirect to ProductBD.update() to change values in database

---
+++ src/store/ProductStore.js
@@ -0,0 +1,57 @@
+import Product from "../model/products/Product";
+import ProductBD from "../model/products/ProductBD";
+import NotExistProduct from "../validators/store/products/NotExistProduct";
+
+class ProductStore {
+    static #Products = [];
+
+    static async getAll(){
+        if(!(this.#Products.length)){
+            const dataProducts = await ProductBD.get();
+            dataProducts.forEach(product => {
+                this.#add(product);
+            });
+            return this.#Products;
+        }
+        return this.#Products;
+    }
+    
+    static get(value,attr = 'id'){
+        try {
+            const [objOrNot] = this.#Products.filter(product => product[attr] === value);
+            NotExistProduct.exec(objOrNot);
+            return objOrNot;
+        } catch (e){
+            throw e;
+        }
+    }
+
+    static #add(objProduct){
+        const newProductInstance = new Product(objProduct);
+        this.#Products.push(newProductInstance);
+        return newProductInstance;
+        //console.log();
+    }
+    
+    static save(objProduct){
+        try {
+            const newProduct = this.#add(objProduct);
+            ProductBD.save(newProduct);  
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    static update(id, property, value){
+        try {
+            const productToChange = this.get(id);
+            productToChange[property] = value;
+            ProductBD.update(productToChange);
+            
+        } catch (e) {
+            throw e;
+        }
+    }
+}
+
+export default ProductStore;

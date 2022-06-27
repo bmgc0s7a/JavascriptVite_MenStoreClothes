e4fd4a154a6fe09fb5d842db385d90ba0cc69ea4
Ricardo <ricardo.sousa13@hotmail.com>
2022-06-27 11:48:36 +0100

    added a id in each li in menu and removed flex from app and aboutUS div. added logic to main.js to switch properly between pages

--- main.js
+++ main.js
@@ -12,6 +12,23 @@ document.addEventListener('insertPage', function(e){
   e.detail.elements.forEach(element => pagesDiv[e.detail.div].append(element)); 
 });
 
+const selectPage = document.querySelector('#menu')
+selectPage.addEventListener('click', function(e){
+  const menuActive = e.target.closest('li')
+  if(menuActive)
+  {
+    const divShop = document.querySelector('#app');
+
+    [...divShop.children].forEach(div => {
+      if(menuActive.id.includes(div.id)){
+        if(div.classList.contains('hidden')) div.classList.remove('hidden')
+      }
+      else{
+        div.classList.add('hidden')
+      }
+    })
+  }
+})
+
 renderPages();
 
-console.log(pagesDiv);

5907f17e5fc2178dfbc3f3bc2287280f3a4e8fe5
Ricardo <ricardo.sousa13@hotmail.com>
2022-06-28 15:28:34 +0100

    added new proptery name to both widgets input and textarea

--- src/view/components/widgets/input.js
+++ src/view/components/widgets/input.js
@@ -1,6 +1,7 @@
-const input = function(type, placeholder, classList = []){
+const input = function(type, name, placeholder, classList = []){
     const input = document.createElement('input');
     input.type = type;
+    input.name= name;
     (input.type === 'submit') ? input.value = placeholder : input.placeholder = placeholder;
     classList.forEach(classe => input.classList.add(classe));
     return input;

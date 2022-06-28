e3885afc421cd2652347144b4d8634935362b598
Stawerr <renato-silva_16@hotmail.com>
2022-06-28 22:13:43 +0100

    h1 widget created

--- src/view/components/cart/h1.js
+++
@@ -1,8 +0,0 @@
-const h1 = function () {
-    const h1Events = document.createElement("h1");
-    h1Events.textContent = 'CURRENT SHOPPING CART';
-    h1Events.classList.add('font-kaushan');
-    return h1Events;
-  };
-  
-  export { h1 };

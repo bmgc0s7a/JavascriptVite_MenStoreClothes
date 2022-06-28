61ada28f71afee15d1f0f227bbfda087392ee6c9
Ricardo <ricardo.sousa13@hotmail.com>
2022-06-28 15:27:25 +0100

    added classes to divs and inputs and created event to prevent empty inputs

--- src/view/components/contact/mapContactUs.js
+++ src/view/components/contact/mapContactUs.js
@@ -1,6 +1,7 @@
+import {p} from "../widgets/p.js";
+
 const mapContactUs = function(){
     const map = L.map('map').setView([51.5295177,-0.2596138], 13);
-    map.classList.add("mapContact")
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '© OpenStreetMap'
@@ -8,6 +9,7 @@ const mapContactUs = function(){
     
     const marker = L.marker([51.5295177,-0.2596138]).addTo(map);
     marker.bindPopup("<b>BOTIQUE BUSHIDO</b>").openPopup();
+    p('The Chase Centre, 2 Chase Rd, London NW10 6HZ, United Kingdom')
 }
 
 export {mapContactUs}

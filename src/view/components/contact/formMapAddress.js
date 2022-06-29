1e339e8f5d6fc505a8fbac429dc3c19f4a9979f9
Ricardo <ricardo.sousa13@hotmail.com>
2022-06-29 11:36:32 +0100

    deletes links in header from leaflet and installed leaflet with npm and created two divs appended them

--- src/view/components/contact/formMapAddress.js
+++ src/view/components/contact/formMapAddress.js
@@ -4,12 +4,11 @@ import { p } from "../widgets/p.js";
 
 function formMapAddress() {
     const divMapForm = document.createElement('div')
-    divMapForm.classList.add("flex","flex-col-reverse", "mx-8","sm:flex-row", "map", "mapContact", "my-8", "justify-center",  "bg-amber-100" )
-    divMapForm.id = 'map'
+    divMapForm.classList.add("flex", "flex-col-reverse", "mx-8", "sm:flex-row", "map", "mapContact", "my-8", "justify-center", "bg-amber-100")
 
     divMapForm.append(
         formContactUs(),
-        p('map')
+        mapContactUs()
     )
 
     return divMapForm

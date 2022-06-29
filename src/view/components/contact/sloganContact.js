626b79d525b0f4beb2ac2efeadf554d750b50ac3
Miguel Lima <miguel.lima.t0121546@edu.atec.pt>
2022-06-29 16:16:12 +0100

    Added new div component slogan, class bg-slogan in Contact page

--- src/view/components/contact/sloganContact.js
+++ src/view/components/contact/sloganContact.js
@@ -1,15 +1,28 @@
 import { h1 } from "../widgets/h1";
 
 const sloganContact = function(){
-    // Criar div
     
-    // Criar Frase
+    const divSlogan = document.createElement('div');
+    divSlogan.classList.add('bg-slogan', 'md:h-[500px]', 'h-80', 'flex', 'justify-end', 'flex-col', 'items-center', 'p-7');
+    const wordSlogan = ['Be Wise', 'Be Patient', 'Be Serene'];
+    const h1Slogan = h1(
+        'Let Your Essence And Lifestyle OverFlow', ['text-center', 'text-amber-100', 'uppercase', 'text-2xl']
+    );
+    const h1SloganNext = h1(
+        wordSlogan.at(0), ['text-amber-100', 'uppercase', 'text-4xl', 'p-3']
+    );
+    let indexSlogan = 1;
 
-    // Criar Texto que roda
-
-    // Append da frase e do texto na div
+    setInterval(()=>{
+        if(indexSlogan == 3) indexSlogan = 0
+        h1SloganNext.textContent = wordSlogan.at(indexSlogan);
+        indexSlogan++;
+    }, 5000)
     
-    // return "<p>dadasd";
+
+    divSlogan.append(h1Slogan,h1SloganNext)
+
+    return divSlogan;
 }
 
 export {sloganContact};

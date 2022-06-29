fdf2022852e047c657fc046504fd9284331cdc00
bmgcosta <bmgcosta@outlook.com>
2022-06-29 15:04:22 +0100

    Change some classes in div Contact Us

--- src/view/components/contact/contactUs.js
+++ src/view/components/contact/contactUs.js
@@ -3,10 +3,10 @@ import { formMapAddress } from "./formMapAddress.js"
 
 function ContactUs() {
     const divContactUs= document.createElement('div')
-    divContactUs.classList.add("flex", "flex-col", "bg-amber-900" , "justify-center", "contactUs")
+    divContactUs.classList.add("flex", "flex-col", "bg-amber-900" , "justify-center", "contactUs", "p-10")
 
     divContactUs.append(
-        h1('CONTACT US', ["text-center", "mt-2" , "text-lg", "font-nunito"]),
+        h1('CONTACT US', ["text-center", "mt-2" , "text-2xl", 'text-amber-900' , "font-nunito"]),
         formMapAddress(),
     )
 
--- src/view/components/contact/formMapAddress.js
+++ src/view/components/contact/formMapAddress.js
@@ -4,8 +4,8 @@ import { p } from "../widgets/p.js";
 
 function formMapAddress() {
     const divMapForm = document.createElement('div')
-    divMapForm.classList.add("flex", "flex-col-reverse", "mx-8", "p-5", "md:flex-row", "map", "mapContact", "my-8", "justify-center", "bg-amber-100", "gap-3", "rounded-md")
-
+    divMapForm.classList.add("flex", "flex-col-reverse", "mx-8", "p-5", "md:flex-row", "map", "mapContact", "my-3", "justify-center", "bg-amber-100", "gap-3", "rounded-md")
+    
     divMapForm.append(
         formContactUs(),
         mapContactUs()

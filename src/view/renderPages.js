5860b2eb18145e39fc3c091a0919f6b8aa253f05
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-01 12:00:23 +0100

    accepted both changes in renderPage

--- src/view/renderPages.js
+++ src/view/renderPages.js
@@ -4,12 +4,9 @@ import { itemCart } from "./components/cart/itemCart.js";
 import { sloganContact } from "./components/contact/sloganContact.js";
 import { cardShop } from "./components/shop/card.js";
 import { formCoupon } from "./components/cart/coupon.js";
-<<<<<<< HEAD
 import { searchAndCartBtn } from "./components/shop/searchAndCartBtn.js";
-=======
 import { Payment } from "./components/cart/payment.js";
 import { workerComponent } from "./components/contact/workerComponent.js";
->>>>>>> a43faa1fd6cb8b87e8b811fd354e02f19e75efb5
 
 const renderPages = function(){
 

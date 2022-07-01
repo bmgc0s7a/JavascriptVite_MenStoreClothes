a03b45a2627ee0ec48bc1cde86c3f01cfa0afdb4
Stawerr <renato-silva_16@hotmail.com>
2022-07-01 13:25:04 +0100

    created icons of payments type on payment component

--- src/view/components/cart/payment.js
+++ src/view/components/cart/payment.js
@@ -1,6 +1,6 @@
 import { p } from "../widgets/p.js";
 import { button } from "../widgets/button.js";
-import { buttonIcon } from "../widgets/buttonIcon.js";
+import {icon } from "../widgets/icon.js";
 
 function payment() {
     const divPayment = document.createElement("div");
@@ -15,9 +15,14 @@ function payment() {
         p('10€',['text-end']),
         p('Total:'),
         p('20€',['text-end']),
-        button('Purchase', 'Make Purchase', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-10", "placeholder-opacity-50" , "text-xs","col-span-2"]), 
+        button('Purchase', 'Make Purchase', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-10", "placeholder-opacity-50","col-span-2"]), 
     )
-    divPaymentLogos.append()
+    divPaymentLogos.append(
+        icon(["fa-brands", "fa-cc-mastercard","pt-2","px-2","text-3xl","text-amber-900"]),
+        icon(["fa-brands", "fa-cc-paypal","pt-2","px-2","text-3xl","text-amber-900" ]),
+        icon(["fa-brands", "fa-cc-visa","pt-2","px-2","text-3xl","text-amber-900" ]),
+        icon(["fa-brands", "fa-cc-amex","pt-2","px-2","text-3xl","text-amber-900" ])
+        )
     divPayment.classList.add('flex','flex-col','bg-white','my-4','justify-center','items-center','p-4')
     divPayment.append(divPaymentText,divPaymentLogos)
 

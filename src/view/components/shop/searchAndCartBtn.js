b834bcadac2094debdfd958a9c09a5104a4b5035
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-01 11:55:24 +0100

    created div that has search bar and button to cart, styled them with tailwind and implemented search icon

--- src/view/components/shop/searchAndCartBtn.js
+++ src/view/components/shop/searchAndCartBtn.js
@@ -1,10 +1,11 @@
 import { button } from "../widgets/button.js"
+import { icon } from "../widgets/icon.js"
 import { input } from "../widgets/input.js"
 
 function searchAndCartBtn() {
 const divSearchBtn = document.createElement('div')
-divSearchBtn.classList.add()
-const cartBtn = button("cartBtn","Ir para o carrinho", ["bg-amber-700"])
+divSearchBtn.classList.add("flex", "items-center", "gap-8", "min-w-full", "place-content-around")
+const cartBtn = button("cartBtn","Ir para o carrinho", ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-11", "placeholder-opacity-50" , "text-xs"] )
 cartBtn.addEventListener('click', (e)=>{
 
         const divShop = document.querySelector('#app');
@@ -18,11 +19,18 @@ cartBtn.addEventListener('click', (e)=>{
           }
         })
       
-    
-
+       
 })
+
+ const divSearch = document.createElement('div')
+        divSearch.classList.add()
+        divSearch.append(
+            icon(["fa-solid", "fa-magnifying-glass" ]),
+            input("text","searchBar", "Search Item",["bg-amber-100", "placeholder-black", "placeholder-opacity-50", "border-b-2", "border-black", "focus:outline-none"])
+        )
+
 divSearchBtn.append(
-    input("text","searchBar", "Search Item"),
+   divSearch,
     cartBtn
 )
 

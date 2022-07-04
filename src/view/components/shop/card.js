c07b2f3c465d8da7d0989d69b097ebb00dc26ec7
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-04 10:53:30 +0100

    created some logic in searchAndCartBtn that calls the event of filter Product that's called in card.js

--- src/view/components/shop/card.js
+++ src/view/components/shop/card.js
@@ -1,10 +1,11 @@
 import ProductStore from "../../../store/ProductStore.js"
 import { descriptionCard } from "./descriptionCard.js"
+import { filterProduct } from "./events/filterProducts.js";
 import { imageCard } from "./imageCard.js"
 
 function cardShop() {
     const divCardShop = document.createElement('div');
-    divCardShop.classList.add("grid","grid-cols-1","sm:grid-cols-2", "lg:grid-cols-4", "p-6", "gap-4");
+    divCardShop.classList.add("grid", "grid-cols-1", "sm:grid-cols-2", "lg:grid-cols-4", "p-6", "gap-4");
 
 
     (async () => {
@@ -12,12 +13,12 @@ function cardShop() {
 
         products.forEach(product => {
             const divOneCard = document.createElement('div')
-            divOneCard._id=product.id
-            divOneCard._title=product.title
-            divOneCard.classList.add("product","flex", "flex-col","gap-4", "items-center","bg-white" ,"max-w-[250px]", "rounded-md", "pt-4", "justify-between")
+            divOneCard._id = product.id
+            divOneCard._title = product.title
+            divOneCard.classList.add("product", "flex", "flex-col", "gap-4", "items-center", "bg-white", "max-w-[250px]", "rounded-md", "pt-4", "justify-between")
             divOneCard.append(
                 imageCard(product.image),
-                descriptionCard(product.title,product.desc, product.price)
+                descriptionCard(product.title, product.desc, product.price)
             )
             divCardShop.append(
                 divOneCard
@@ -25,8 +26,9 @@ function cardShop() {
         })
     })();
 
-
+    document.addEventListener('filterProduct', (e) => filterProduct(e,divCardShop) )
     return divCardShop
 }
+
 
 export { cardShop }
--- src/view/components/shop/searchAndCartBtn.js
+++ src/view/components/shop/searchAndCartBtn.js
@@ -4,7 +4,7 @@ import { input } from "../widgets/input.js"
 
 function searchAndCartBtn() {
 const divSearchBtn = document.createElement('div')
-divSearchBtn.classList.add("flex", "items-center", "gap-8", "min-w-full", "place-content-around")
+divSearchBtn.classList.add("flex","flex-wrap", "items-center", "gap-8", "min-w-full", "justify-center")
 const cartBtn = button("cartBtn","Ir para o carrinho", ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-11", "placeholder-opacity-50" , "text-xs"] )
 cartBtn.addEventListener('click', (e)=>{
 
@@ -22,17 +22,21 @@ cartBtn.addEventListener('click', (e)=>{
        
 })
 
- const divSearch = document.createElement('div')
-        divSearch.classList.add()
-        divSearch.append(
-            icon(["fa-solid", "fa-magnifying-glass" ]),
-            input("text","searchBar", "Search Item",["bg-amber-100", "placeholder-black", "placeholder-opacity-50", "border-b-2", "border-black", "focus:outline-none"])
-        )
+const inputSearch = input("text","searchBar", "Search Item",["bg-amber-100", "placeholder-black", "placeholder-opacity-50", "border-b-2", "border-black", "focus:outline-none", "grow", "max-w-lg"]);
+
+inputSearch.addEventListener('keyup', (e)=>{
+
+  document.dispatchEvent(new CustomEvent('filterProduct',{detail:e.target.value}))
+} )
+
 
 divSearchBtn.append(
-   divSearch,
+  icon(["fa-solid", "fa-magnifying-glass" ]),
+  inputSearch,
     cartBtn
 )
+
+
 
 return divSearchBtn
 }

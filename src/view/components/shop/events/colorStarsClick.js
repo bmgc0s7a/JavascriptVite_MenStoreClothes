628ded340254539c58494487b1b3cff8e00fdc52
bmgcosta <bmgcosta@outlook.com>
2022-07-05 11:15:46 +0100

    Render start with rate and change color or not

---
+++ src/view/components/shop/events/colorStarsClick.js
@@ -0,0 +1,13 @@
+const colorStarsClick = function (divStart, rate){
+    const divsStars = divStart.childNodes;
+    divsStars.forEach(star => {
+        const [svg] = star.childNodes;
+        console.log(star);
+        if(star._id <= rate) 
+            svg.classList.add('text-yellow-500')
+        else
+            svg.classList.remove('text-yellow-500');
+    });
+}
+
+export {colorStarsClick};

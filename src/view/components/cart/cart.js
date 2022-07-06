747093811c97fc112cb837301be9e415d85c78aa
Stawerr <renato-silva_16@hotmail.com>
2022-07-06 12:27:13 +0100

    added logic to cart actions buttons and logic to know if the cart is full or empty

--- src/view/components/cart/cart.js
+++ src/view/components/cart/cart.js
@@ -2,6 +2,8 @@ import { title } from "./title.js";
 import { coupon } from "./coupon.js";
 import { payment } from "./payment.js";
 import { itemCart } from "./itemCart.js";
+import CartStore from "../../../store/CartStore.js";
+import { p } from "../widgets/p.js";
 
 
 function cart() {
@@ -9,15 +11,45 @@ function cart() {
     const divCart = document.createElement("div");
     const divTitleItem = document.createElement("div");
     const divCouponPayment = document.createElement("div");
+    const empty = p('Your cart is empty!')
+    const divItems = document.createElement("div");
+    const itemsDB = CartStore.get()
 
 
-    divTitleItem.append(title(),itemCart())
-    divCouponPayment.append(coupon(),payment());
+    divCouponPayment.append(coupon(), payment());
+    divCart.append(empty)
+    if (!itemsDB) {
+        divTitleItem.classList.add('hidden')
+        divCouponPayment.classList.add('hidden')
+        empty.id = "cart-empty"
 
-    divCart.classList.add('flex', 'flex-col','lg:flex-row','justify-between');
-    divCart.append(divTitleItem,divCouponPayment)
+    } else {
+        empty.classList.add('hidden')
+        itemsDB.products.forEach(product => {
+            divItems.append(itemCart(product))
+        });
+    }
+
+    divTitleItem.append(title(), divItems)
+
+    document.addEventListener('cartEmpty', function () {
+        divTitleItem.classList.add('hidden')
+        divCouponPayment.classList.add('hidden')
+        empty.classList.remove('hidden')
+    })
+
+    document.addEventListener('productAdd', function () {
+        if (divTitleItem.classList.contains('hidden')) {
+            divTitleItem.classList.remove('hidden')
+            divCouponPayment.classList.remove('hidden')
+            empty.classList.add('hidden')
+        }
+    })
+
+    divCart.classList.add('flex', 'flex-col', 'lg:flex-row', 'justify-between');
+    divCart.append(divTitleItem, divCouponPayment)
 
     return divCart;
 }
 
-export {cart}
+export { cart }
--- src/view/components/cart/itemCart.js
+++ src/view/components/cart/itemCart.js
@@ -1,43 +1,77 @@
 import { img } from "../widgets/img.js";
 import { p } from "../widgets/p.js";
 import { button } from "../widgets/button.js";
-import {buttonIcon } from "../widgets/buttonIcon.js";
+import { buttonIcon } from "../widgets/buttonIcon.js";
+import ProductStore from "../../../store/ProductStore.js";
+import CartStore from "../../../store/CartStore.js";
 
-function itemCart() {
-    const divItem= document.createElement('div')
+function itemCart({ id, quantidade }) {
 
-    const divProductsImg= document.createElement('div')
-    const divProductsDesc= document.createElement('div')
+    const divItem = document.createElement('div')
+    const divProductsImg = document.createElement('div')
+    const divProductsDesc = document.createElement('div')
+
+    //console.log(id,quantidade)
+
+    //const product=ProductStore.get(1)
 
     divProductsImg.classList.add('pr-8')
     divProductsImg.append(
-        img('../src/view/images/artigoTeste.jpg',['w-20']))
-    
+        img('../src/view/images/artigoTeste.jpg', ['w-20']))
+
     divProductsDesc.append(
-        p('H&M',['text-sm','text-slate-600']),
-        p('30€',['font-bold','text-xl']))
+        p('H&M', ['text-sm', 'text-slate-600']),
+        p('30€', ['font-bold', 'text-xl']))
 
-    divProductsDesc.classList.add('flex','flex-col','self-center')
+    divProductsDesc.classList.add('flex', 'flex-col', 'self-center')
 
-    const ElementsLeft= document.createElement('div')
-    ElementsLeft.classList.add("flex", "flex-row", "nunito",'p-4')
-    ElementsLeft.append(divProductsImg,divProductsDesc)
+    const ElementsLeft = document.createElement('div')
+    ElementsLeft.classList.add("flex", "flex-row", "nunito", 'p-4')
+    ElementsLeft.append(divProductsImg, divProductsDesc)
 
-    const divActions= document.createElement('div')
+    const divActions = document.createElement('div')
+    const less = button('less', '-', ["bg-amber-300", "hover:bg-amber-900", "text-amber-900", "hover:text-white", 'border-solid', 'border-2', 'border-amber-900', 'py-1', 'px-2', 'rounded-l-md', 'text-xl'])
+    const plus = button('plus', '+', ["bg-amber-300", "hover:bg-amber-900", "text-amber-900", "hover:text-white", 'border-solid', 'border-2', 'border-amber-900', 'py-1', 'px-2', 'rounded-r-md', 'text-xl'])
+    const del = buttonIcon('delete', ['fa-solid', 'fa-trash'], '', ["bg-amber-300", "hover:bg-amber-900", "text-amber-900", "hover:text-white", 'border-solid', 'border-2', 'border-amber-900', 'py-1', 'px-2', 'ml-4', 'rounded', 'text-xl'])
+    const qtd = button('quantity', quantidade, ["bg-amber-300", "hover:bg-amber-900", "text-amber-900", "hover:text-white", 'border-solid', 'border-y-2', 'border-amber-900', 'py-1', 'px-2', 'text-xl'])
+    qtd.value = quantidade
+    if (qtd.value == 1) {
+        less.disabled = true
+    }
+    less.addEventListener('click', function () {
+        CartStore.addProduct(id, 1, false)
+        qtd.value = qtd.value - 1
+        qtd.textContent = qtd.value
+        if (qtd.value == 1) {
+            less.disabled = true
+        }
+    })
+    plus.addEventListener('click', function () {
+        CartStore.addProduct(id, 1, true)
+        qtd.value = +qtd.value + 1
+        qtd.textContent = qtd.value
+        if (qtd.value > 1) {
+            less.disabled = false
+        }
+        console.log(qtd.value)
+    })
+    del.addEventListener('click', function () {
+        CartStore.delProduct(id)
+    })
 
     divActions.append(
-        button('less','-',["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white",'border-solid', 'border-2', 'border-amber-900','py-1','px-2','rounded-l-md','text-xl']),
-        button('quantity','Qtd',["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white",'border-solid', 'border-y-2', 'border-amber-900','py-1','px-2','text-xl']),
-        button('plus','+',["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white",'border-solid', 'border-2', 'border-amber-900','py-1','px-2','rounded-r-md','text-xl']),
-        buttonIcon('delete',['fa-solid','fa-trash'],'',["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white",'border-solid', 'border-2', 'border-amber-900','py-1','px-2','ml-4','rounded','text-xl']))
-    
-    divActions.classList.add('self-center','text-amber-900',"sm:px-4")
+        less,
+        qtd,
+        plus,
+        del
+    )
+    divActions.classList.add('self-center', 'text-amber-900', "sm:px-4")
 
-    const hr= document.createElement('hr')
-    hr.classList.add('my-2','sm:hidden')
+    const hr = document.createElement('hr')
+    hr.classList.add('my-2', 'sm:hidden')
 
-    divItem.classList.add('flex',"bg-white",'flex-col','justify-center','items-center',"sm:flex-row","sm:justify-between")
-    divItem.append(ElementsLeft,divActions,hr)    
+    divItem.classList.add('flex', "bg-white", 'flex-col', 'justify-center', 'items-center', "sm:flex-row", "sm:justify-between")
+    divItem.append(ElementsLeft, divActions, hr)
 
     return divItem
 }

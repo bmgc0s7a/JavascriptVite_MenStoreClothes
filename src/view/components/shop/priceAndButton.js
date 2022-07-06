6952931ab24e104852162f266dc074eacb9dd847
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-06 11:04:17 +0100

    created a customEvent to updated a product in cart

--- src/view/components/shop/priceAndButton.js
+++ src/view/components/shop/priceAndButton.js
@@ -5,21 +5,26 @@ import { p } from "../widgets/p.js";
 
 const priceAndButton = function (price) {
     const priceButtonDiv = document.createElement('div');
-    priceButtonDiv.classList.add('flex', 'justify-between' , 'items-center');
-    
-const buttonAddCart =  button('addCartButton', 'Add to cart', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-1", "placeholder-opacity-50" , "text-xs"] )
-    
-  buttonAddCart.addEventListener('click', function(e){
-    const productId =e.target.closest('.product')._id
-      CartStore.addProduct(productId)
+    priceButtonDiv.classList.add('flex', 'justify-between', 'items-center');
+
+    const buttonAddCart = button('addCartButton', 'Add to cart', ["bg-amber-300", "hover:bg-amber-900", "text-amber-900", "hover:text-white", "hover:cursor-pointer", "rounded-md", "border-2", "border-amber-900", "placeholder-black", "py-2", "px-1", "placeholder-opacity-50", "text-xs"])
+
+    buttonAddCart.addEventListener('click', function (e) {
+        const productId = e.target.closest('.product')._id
+        CartStore.addProduct(productId)
+        document.dispatchEvent(new CustomEvent('productAdd',
+         {
+           detail:productId
+         }
+        ))
     })
 
-priceButtonDiv.append(
-        p(price+"€", ["font-kaushan", "text-lg"]),
+    priceButtonDiv.append(
+        p(price + "€", ["font-kaushan", "text-lg"]),
         buttonAddCart
     )
 
     return priceButtonDiv
 }
 
-export {priceAndButton}
+export { priceAndButton }

6138ed542871820453cfc2b51250cece52924fd8
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-06 10:39:58 +0100

    created a event in add to cart button that sends products do local store

--- src/view/components/shop/priceAndButton.js
+++ src/view/components/shop/priceAndButton.js
@@ -1,4 +1,5 @@
 
+import CartStore from "../../../store/CartStore.js";
 import { button } from "../widgets/button.js";
 import { p } from "../widgets/p.js";
 
@@ -6,11 +7,18 @@ const priceAndButton = function (price) {
     const priceButtonDiv = document.createElement('div');
     priceButtonDiv.classList.add('flex', 'justify-between' , 'items-center');
     
+const buttonAddCart =  button('addCartButton', 'Add to cart', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-1", "placeholder-opacity-50" , "text-xs"] )
+    
+  buttonAddCart.addEventListener('click', function(e){
+    const productId =e.target.closest('.product')._id
+      CartStore.addProduct(productId)
+    })
 
-    priceButtonDiv.append(
+priceButtonDiv.append(
         p(price+"€", ["font-kaushan", "text-lg"]),
-        button('addCartButton', 'Add to cart', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-1", "placeholder-opacity-50" , "text-xs"] )
+        buttonAddCart
     )
+
     return priceButtonDiv
 }
 

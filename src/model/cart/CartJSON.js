e398381f384cc01bfdc2541bba908e7ac79ff78e
bmgcosta <bmgcosta@outlook.com>
2022-07-07 12:07:15 +0100

    Created method convertAPI convert obj to send API

--- src/model/cart/CartJSON.js
+++ src/model/cart/CartJSON.js
@@ -22,6 +22,26 @@ class CartJSON{
             throw e;
         }
     }
+
+    static convertAPI(cart){
+        try{
+            const products = [];
+            cart.products.forEach(product => 
+                products.push({
+                id: product.id,
+                quantidade: product.quantidade
+            }));
+            const cartObj = {};
+            if(cart.coupon)
+                cartObj.couponcode = cart.coupon.code
+            cartObj.amount = Number(cart.amount.toFixed(2)),
+            cartObj.products = products
+
+            return cartObj;
+        } catch (e){
+            throw e;
+        }
+    }
 }
 
 export default CartJSON;

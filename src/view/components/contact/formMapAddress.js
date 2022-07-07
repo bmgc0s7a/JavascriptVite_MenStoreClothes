5368bf66cf67933f79640a9d9acff9c5620a0741
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-07 17:32:12 +0100

    adjusted when map and form folds into flex-row

--- src/view/components/contact/formMapAddress.js
+++ src/view/components/contact/formMapAddress.js
@@ -4,7 +4,7 @@ import { p } from "../widgets/p.js";
 
 function formMapAddress() {
     const divMapForm = document.createElement('div')
-    divMapForm.classList.add("flex", "flex-col-reverse", "mx-8", "p-5", "md:flex-row", "map", "mapContact", "my-3", "justify-center", "bg-amber-100", "gap-3", "rounded-md")
+    divMapForm.classList.add("flex", "flex-col-reverse", "mx-8", "p-5", "lg:flex-row", "map", "mapContact", "my-3", "justify-center", "bg-amber-100", "gap-3", "rounded-md")
     
     divMapForm.append(
         formContactUs(),

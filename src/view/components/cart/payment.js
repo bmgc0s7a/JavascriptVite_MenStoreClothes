370e01de81c5b5192621d782145855e792f846d0
bmgcosta <bmgcosta@outlook.com>
2022-07-07 12:12:35 +0100

    Create purchaseButton with component button and add new event click

--- src/view/components/cart/payment.js
+++ src/view/components/cart/payment.js
@@ -2,6 +2,8 @@ import { p } from "../widgets/p.js";
 import { button } from "../widgets/button.js";
 import {icon } from "../widgets/icon.js";
 import CartStore from "../../../store/CartStore.js";
+import { messageToUser } from "../public/messageToUser.js";
+import CartBD from "../../../model/cart/CartBD.js";
 
 function payment() {
     const divPayment = document.createElement("div");
@@ -16,6 +18,22 @@ function payment() {
     const totalFinal = p('20€',['text-end']);
     totalFinal.id = "total"
 
+    const purchaseButton = button('Purchase', 'Make Purchase', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-10", "placeholder-opacity-50","col-span-2"]);
+
+    purchaseButton.addEventListener('click', () => {
+        try {
+            (async () => {
+                const [_, status] = await CartStore.payment();
+                messageToUser('Thank you for your purchase!', (status) ?? 'success');
+                CartBD.delete();
+                document.dispatchEvent(new Event('cartEmpty'));
+
+            })()
+        } catch (error) {
+            messageToUser('Fail')
+        }
+    });
+
     document.addEventListener('updatedPrices', (e) =>{
         const price = CartStore.get().amount
         total.textContent = Number(price).toFixed(2)+"€";
@@ -25,9 +43,6 @@ function payment() {
             const price = CartStore.get().amount;
             const priceDiscount = Number(price * (discountDetails.discount / 100)).toFixed(2);
 
-            if(!divPaymentText.querySelector('#discount')){
-                divPaymentText.prepend(p('Discount:'), discount)
-            }
             if(!divPaymentText.querySelector('#discount')){
                 divPaymentText.prepend(p('Discount:'), discount)
             }
@@ -44,7 +59,7 @@ function payment() {
     divPaymentText.append(
         p('Total:'),
         total,
-        button('Purchase', 'Make Purchase', ["bg-amber-300", "hover:bg-amber-900","text-amber-900","hover:text-white","hover:cursor-pointer", "rounded-md" , "border-2", "border-amber-900", "placeholder-black", "py-2", "px-10", "placeholder-opacity-50","col-span-2"]), 
+        purchaseButton
     )
     divPaymentLogos.append(
         icon(["fa-brands", "fa-cc-mastercard","pt-2","px-2","text-3xl","text-amber-900"]),

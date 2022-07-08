45f7f7ea6b675635fb9ad293e222be27907eccd0
Stawerr <renato-silva_16@hotmail.com>
2022-07-08 11:58:49 +0100

    set message time to 900 at messageToUser component

--- src/view/components/public/messageToUser.js
+++ src/view/components/public/messageToUser.js
@@ -2,7 +2,7 @@ import { buttonIcon } from "../widgets/buttonIcon";
 import { h1 } from "../widgets/h1";
 import { removeDiv } from "./events/removeDiv";
 
-const messageToUser = function(message, type = 'error', time = 5000){
+const messageToUser = function(message, type = 'error', time = 900){
     const divMessage = document.createElement('div');
     divMessage.classList.add('fixed','flex','gap-2', 'z-10','right-0', 'top-5', 'p-4' , (type === 'error') ? 'bg-red-600' : 'bg-green-600', 'cursor-pointer');
     const h1Elem = h1(message);

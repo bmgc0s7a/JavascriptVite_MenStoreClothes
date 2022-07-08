d5fe3c96a058dd6e388c4f97ece1734bc36f83dc
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-08 11:21:27 +0100

    worked in cart with class justify-between and justify-center when cart with or without products

--- src/view/components/cart/cart.js
+++ src/view/components/cart/cart.js
@@ -3,31 +3,37 @@ import { coupon } from "./coupon.js";
 import { payment } from "./payment.js";
 import { itemCart } from "./itemCart.js";
 import CartStore from "../../../store/CartStore.js";
-import { p } from "../widgets/p.js";
+import { divEmptyCart } from "./EmptyCart.js";
 
 
 function cart() {
+    
 
     const divCart = document.createElement("div");
     const divTitleItem = document.createElement("div");
     const divCouponPayment = document.createElement("div");
-    const empty = p('Your cart is empty!')
     const divItems = document.createElement("div");
     const itemsDB = CartStore.get()
 
+    divCart.classList.add('flex', 'flex-col', 'lg:flex-row', 'justify-between');
+
+    const divCartEmpty = divEmptyCart()
 
     divCouponPayment.append(coupon(), payment());
-    divCart.append(empty)
+    divCart.append(divCartEmpty)
     if (!itemsDB) {
         divTitleItem.classList.add('hidden')
         divCouponPayment.classList.add('hidden')
-        empty.id = "cart-empty"
+        divCart.classList.remove('justify-between')
+        divCart.classList.add('justify-center')
 
     } else {
-        empty.classList.add('hidden')
+        divCartEmpty.classList.add('hidden')
         itemsDB.products.forEach(product => {
             divItems.append(itemCart(product))
         });
+        divCart.classList.add('justify-between')
+        divCart.classList.remove('justify-center')
     }
 
     divTitleItem.append(title(), divItems)
@@ -36,21 +42,27 @@ function cart() {
         if(!divItems.childElementCount){
             divTitleItem.classList.add('hidden')
             divCouponPayment.classList.add('hidden')
-            empty.classList.remove('hidden')
+            divCartEmpty.classList.remove('hidden')
+            divCart.classList.remove('justify-between')
+        divCart.classList.add('justify-center')
         }
     })
 
     document.addEventListener('cartEmpty', function () {
         divTitleItem.classList.add('hidden')
         divCouponPayment.classList.add('hidden')
-        empty.classList.remove('hidden')
+        divCartEmpty.classList.remove('hidden')
+        divCart.classList.remove('justify-between')
+        divCart.classList.add('justify-center')
     });
 
     document.addEventListener('productAdd', function (e) {
         if (divTitleItem.classList.contains('hidden')) {
             divTitleItem.classList.remove('hidden')
             divCouponPayment.classList.remove('hidden')
-            empty.classList.add('hidden')
+            divCartEmpty.classList.add('hidden')
+            divCart.classList.add('justify-between')
+        divCart.classList.remove('justify-center')
         }
         const [divFind] = [...divItems.childNodes].filter(divProduct => divProduct._id === e.detail);
         if(divFind)
@@ -63,7 +75,7 @@ function cart() {
             divItems.append(itemCart({id: e.detail, quantidade:1}))
     })
 
-    divCart.classList.add('flex', 'flex-col', 'lg:flex-row', 'justify-between');
+    
     divCart.append(divTitleItem, divCouponPayment)
 
     return divCart;

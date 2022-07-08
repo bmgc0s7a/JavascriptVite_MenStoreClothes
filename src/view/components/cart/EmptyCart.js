97864019028d7862d7a86504478fb1162308b770
Ricardo <ricardo.sousa13@hotmail.com>
2022-07-08 11:20:29 +0100

    created a component that inserts a h1 and icon  to cart when empty

---
+++ src/view/components/cart/EmptyCart.js
@@ -0,0 +1,18 @@
+import { h1 } from "../widgets/h1"
+import { icon } from "../widgets/icon"
+
+const divEmptyCart = function () {
+    const divEmptyCart = document.createElement("div");
+    divEmptyCart.id= "cart-empty"
+    divEmptyCart.classList.add("flex", "flex-col", "item-center", "gap-3")
+
+    divEmptyCart.append( 
+        icon(['fa-solid', 'fa-cart-arrow-down', "text-5xl"]),
+        h1('Your cart is empty!')
+       
+    )
+
+    return divEmptyCart
+}
+
+export { divEmptyCart }
